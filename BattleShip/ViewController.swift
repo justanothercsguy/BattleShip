57dad47181f7901404d9aa297e15a52f2e82b0f2
Zishi Wu <salsazishi@gmail.com>
2016-03-03 16:14:40 -0500

    Initial Commit

---
+++ BattleShip/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BattleShip
+//
+//  Created by Zishi Wu on 3/3/16.
+//  Copyright © 2016 Zishi Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
