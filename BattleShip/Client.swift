fe58f8dfcc74c8dfe15955a613b0413cce34611e
Zishi Wu <zishiwu@zishis-macbook-air.local>
2016-03-05 14:36:42 -0500

    Client class added

---
+++ BattleShip/Client.swift
@@ -0,0 +1,31 @@
+//
+//  Client.swift
+//  BattleShip
+//
+//  Created by Zishi Wu on 3/5/16.
+//  Copyright © 2016 Zishi Wu. All rights reserved.
+//
+
+import Foundation
+import SocketIOClientSwift
+
+class Client {
+    
+    let socket = SocketIOClient(socketURL: NSURL(string: "http://localhost:8080")!, options: [.Log(true), .ForcePolling(true)])
+    
+    socket.on("connect") { data, ack in
+        print("socket connected")
+    }
+        
+    socket.on("currentAmount") {data, ack in
+        if let cur = data[0] as? Double {
+            socket.emitWithAck("canUpdate", cur)(timeoutAfter: 0) {data in
+                socket.emit("update", ["amount": cur + 2.50])
+            }
+    
+            ack.with("Got your currentAmount", "dude")
+        }
+    }
+        
+    socket.connect()
+}
