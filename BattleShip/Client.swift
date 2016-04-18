402eeb637cc2543e51877782d1c4343aca543fcf
Zishi Wu <zishiwu@zishis-macbook-air.local>
2016-04-18 15:15:35 -0400

    test

--- BattleShip/Client.swift
+++ BattleShip/Client.swift
@@ -28,7 +28,7 @@ class Client {
     static let sharedInstance = Client()
     
     // change 192.168.1.64 to your local ip address
-    let socket = SocketIOClient(socketURL: NSURL(string: "http://10.223.113.174:3000")!, options: [.Log(false), .ForcePolling(true)])
+    let socket = SocketIOClient(socketURL: NSURL(string: "http://battleship.mybluemix.net/:80")!, options: [.Log(false), .ForcePolling(true)])
     var id: Int!
     var otherPlayerID: Int!
     var gameWon = false
@@ -41,25 +41,11 @@ class Client {
             print("socket connected")
         }
         
-        self.socket.on("updateGameBoard") {[weak self] data, ack in
-            if let gameBoardData = data[0] as? NSDictionary {
-                // go through the message - should be NSArray or NSDictionary
-                for tileCoords in gameBoardData {
-                    
-                }
-            }
-        }
-        
         self.socket.on("clientID") {[weak self] data, ack in
             if let id = data[0] as? Int {
                 self?.id = id
             }
         }
-        /*
-        self.socket.on("won") {data, ack in
-            print("won")
-        }
-*/
         
         self.socket.on("newGameWithOtherPlayer") {[weak self] data, ack in
             // nothing else seems to work, have to do it this way to get the data out
